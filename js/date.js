8dcbd7e2b40bb9caf48fb0f3f99f1e2168bed7df
col17029 <col17029@byui.edu>
2020-01-10 18:20:47 -0500

    master

--- js/date.js
+++ js/date.js
@@ -5,7 +5,7 @@ try {
         month: "long",
         year: "numeric"
     };
-    document.getElementById("currentdate").textContent = new Date().toLocaleDateString("en-US", options);
+    document.getElementById("currentdate").textContent = document.lastModified;
 } catch (e) {
     alert("Error with code or your browser does not support Locale");
 }
