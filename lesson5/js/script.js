a28a17a1e4a1421e2ceebd2ae5fe1812585b882e
col17029 <col17029@byui.edu>
2020-02-07 18:52:19 -0500

    save

--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -5,11 +5,32 @@ try {
         month: "long",
         year: "numeric"
     };
-    document.getElementById("currentdate").textContent = new Date().toLocaleDateString("en-US",  option);
+    document.getElementById("currentdate").textContent = new Date().toLocaleDateString("en-US", option);
 } catch (e) {
     alert("Error with code or your browser does not support Locale");
 }
 
 function togglemenu() {
     document.getElementById("menuNav").classList.toggle("hide");
-  }
+}
+
+/*try {
+    let today = new Date().getDay()
+    if (today == 5) {
+        document.getElementById("noticeText").style.display = "block";
+    } else {
+        document.getElementById("noticeText").style.display = "none";
+    };
+    
+} catch (e) {
+    alert("Error with code or your browser does not support Locale");
+}*/
+
+function showDate() {
+    let today = new Date().getDay()
+    if (today == 5) {
+        document.getElementById("noticeText").style.display = "block";
+    } else {
+        document.getElementById("noticeText").style.display = "none";
+    };
+}
