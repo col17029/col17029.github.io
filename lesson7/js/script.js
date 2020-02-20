86568a04b0866130ea45f6ce5211e6fc6d2f89eb
col17029 <col17029@byui.edu>
2020-02-20 09:59:03 -0500

    save

---
+++ lesson7/js/script.js
@@ -0,0 +1,24 @@
+try {
+    let option = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    };
+    document.getElementById("currentdate").textContent = new Date().toLocaleDateString("en-US", option);
+} catch (e) {
+    alert("Error with code or your browser does not support Locale");
+}
+
+function togglemenu() {
+    document.getElementById("menuNav").classList.toggle("hide");
+}
+
+WebFont.load({
+    google: {
+      families: [
+         'Catamaran',
+         'Barlow Condensed'
+      ]
+    }
+  });
