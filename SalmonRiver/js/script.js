1b3fff8f12b8b88e8efa47ad670e05b3d100f228
col17029 <col17029@byui.edu>
2020-04-01 17:26:32 -0400

    save

---
+++ SalmonRiver/js/script.js
@@ -0,0 +1,29 @@
+WebFont.load({
+    google: {
+      families: [
+         'Raleway'
+      ]
+    }
+  });
+
+
+//-------------------------------------------------------------------
+
+function togglemenu() {
+  document.getElementById("menuNav").classList.toggle("hide");
+}
+
+
+//------------------------------------------------------------------
+
+try {
+  let option = {
+      weekday: "long",
+      day: "numeric",
+      month: "long",
+      year: "numeric"
+  };
+  document.getElementById("currentdate").textContent = new Date().toLocaleDateString("en-US", option);
+} catch (e) {
+  alert("Error with code or your browser does not support Locale");
+}
