5a911c73188a1a4aad9b51999247fa28eaf5601b
col17029 <col17029@byui.edu>
2020-07-25 10:20:13 -0400

    save

--- js/jokes.js
+++ js/jokes.js
@@ -1,64 +1,87 @@
-const url = 'https://icanhazdadjoke.com/';
+//const url = 'https://icanhazdadjoke.com/';
 
-function getJoke(url){
+function getJoke(url) {
 	return fetch(url, {
-		headers: {
-			'accept': 'application/JSON'
-		}
-	})
-	.then(function (response) {
-		if (!response.ok) {
-			throw Error(response.statusText);
-		} else {
-			return response.json();
-		}
-	})
-	//.then(text => console.log(text))
-	.catch(function (error) {
-		console.log(error);
-	});
+			headers: {
+				'accept': 'application/JSON'
+			}
+		})
+		.then(function (response) {
+			if (!response.ok) {
+				throw Error(response.statusText);
+			} else {
+				return response.json();
+			}
+		})
+		.catch(function (error) {
+			console.log(error);
+		});
 }
-// Display Joke ?? Needs work
-function renderJoke(data){
-	console.log(data.joke);
-	console.log(data.id)
-	const jokeText = document.getElementById('joke-text');
-	const jokeID = document.getElementById('joke-id');
-	jokeText.innerHTML = data.joke;
-	jokeID.innerHTML = data.id;
-}
+// Display Joke
+// function renderJoke(data) {
+// 	console.log(data.joke);
+// 	console.log(data.id)
+// 	const jokeText = document.getElementById('joke-text');
+// 	const jokeID = document.getElementById('joke-id');
+// 	jokeText.innerHTML = data.joke;
+// 	jokeID.innerHTML = data.id;
+// }
 
-function showJokes(url = 'https://icanhazdadjoke.com/'){
+function showRandomJoke() {
+	let url = 'https://icanhazdadjoke.com/'
 	getJoke(url).then(function (data) {
 		console.log(data);
-		//const results = data.results;
-
 		// render joke
-		renderJoke(data);
-
+		//renderJoke(data);
+		document.getElementById('joke-text').innerHTML = data.joke;
+		document.getElementById('joke-text').className = "";
+		document.getElementById('joke-table').className = "hidden";
 	});
 }
 
-function searchJokes(searchUrl){
-	getJoke(searchUrl).then(function (data) {
+function showJokeList(jokeTerm) {
+	const term = jokeTerm;
+	let url = 'https://icanhazdadjoke.com/search?term=' + term
+	getJoke(url).then(function (data) {
 		console.log(data);
-		renderJoke(data);
-	})
+		const results = data.results;
+		const jokeListElement = document.getElementById("jokeList");
+		renderJokeList(results, jokeListElement);
+		document.getElementById('joke-text').className = "hidden";
+		document.getElementById('joke-table').className = "";
+		document.getElementById('jokeCount').innerHTML = "Found " + data.total_jokes + " jokes!"; 
+	});
 }
 
+function renderJokeList(jokes, jokeListElement) {
+	//console.table(jokes);
+	const list = jokeListElement;
+	list.innerHTML = "";
+	
+	jokes.forEach(joke => {
+		let listItem = document.createElement("ul");
+		listItem.innerHTML = `
+        	<li>${joke.joke}</li>
+			`;
+		list.appendChild(listItem);
+	});
+	
+};
+
+
 const randomBtn = document.getElementById("random");
-randomBtn.addEventListener("click", function (event){
+randomBtn.addEventListener("click", function (event) {
 	event.preventDefault();
-	showJokes();
+	showRandomJoke();
 });
 
-const btnSearch = document.getElementById("btnSearch");
-btnSearch.addEventListener("click", function (event){
+const searchBtn = document.getElementById("search");
+searchBtn.addEventListener("click", function (event) {
 	event.preventDefault();
 	const jokeTerm = document.getElementById("jokeTerm").value;
 	console.log(jokeTerm);
-	let searchUrl = 'https://icanhazdadjoke.com/search/?term=' + jokeTerm;
-	searchJokes(searchUrl);
+	//let searchUrl = 'https://icanhazdadjoke.com/search/?term=' + jokeTerm;
+	showJokeList(jokeTerm);
 })
 
-showJokes();
+showRandomJoke();
